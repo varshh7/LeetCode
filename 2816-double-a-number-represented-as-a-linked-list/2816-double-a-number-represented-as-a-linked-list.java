c86cca5dbb9ea654fdc259294a7f978ea23b6164
Varsha <varshasoundar4@gmail.com>
2026-01-26 10:27:19 +0530

    Time: 4 ms (81.17%), Space: 48.7 MB (25.8%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
@@ -10,30 +10,31 @@
  */
 class Solution {
     public ListNode doubleIt(ListNode head) {
-        head=reverse(head);
-        ListNode curr=head;
-        int carry=0;
-        while(curr!=null){
-            int sum=curr.val*2 +carry;
-            curr.val=sum%10;
-            carry=sum/10;
-            if(curr.next==null && carry>0){
-                curr.next =new ListNode(carry);
+        head = reverse(head);
+        ListNode curr = head;
+        int carry = 0;
+        while (curr != null) {
+            int sum = curr.val * 2 + carry;
+            curr.val = sum % 10;
+            carry = sum / 10;
+            if (curr.next == null && carry > 0) {
+                curr.next = new ListNode(carry);
                 break;
             }
-            curr=curr.next;
+            curr = curr.next;
 
         }
         return reverse(head);
     }
-    public ListNode reverse(ListNode head){
-        ListNode prev=null;
-        ListNode curr=head;
-        while(curr!=null){
-            ListNode next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
+
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
         return prev;
     }
